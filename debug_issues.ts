d70716bf247adc37ae39bca93d16e8d4cd21d5cb
HoaDepZay <dangquanghoa206@gmail.com>
2026-05-28 19:55:31 +0700

    chore: clean up root directory, move docs to docs/ folder, and remove debug scripts

--- debug_issues.ts
+++
@@ -1,8 +0,0 @@
-import { connectDB, appPool } from "./config/db";
-async function run() {
-    await connectDB();
-    const r = await appPool.request().query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' ORDER BY TABLE_NAME");
-    console.log("All tables:", r.recordset.map((x: any) => x.TABLE_NAME).join(", "));
-    process.exit(0);
-}
-run().catch(e => { console.error(e.message); process.exit(1); });
